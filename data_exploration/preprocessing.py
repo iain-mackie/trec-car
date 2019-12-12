856dcbd7a151407ae0459f514bb7eefa28c2ddd1
iain-mackie <i.mackie@me.com>
2019-12-12 17:58:10 +0000

    testing with data and search

--- data_exploration/preprocessing.py
+++ data_exploration/preprocessing.py
@@ -5,6 +5,8 @@ from trec_car.read_data import iter_paragraphs, ParaText, ParaLink, iter_pages
 
 def preprocess_queries(path=Wiki.file_path_list[0], limit=1):
 
+    queries = []
+
     print('*** reading {} articles from file: {} ***'.format(limit, path))
     with open(path, 'rb') as f:
 
@@ -12,21 +14,27 @@ def preprocess_queries(path=Wiki.file_path_list[0], limit=1):
 
         for p in iter_pages(f):
 
-            print('----------------------- Queries   -----------------------')
+            #print('----------------------- Queries   -----------------------')
             # get one data structure with nested (heading, [children]) pairs
-            headings = p.nested_headings()
+            #headings = p.nested_headings()
 
             if len(p.outline()) > 0:
 
-                print('flat headings= ', ["/".join([str(section.heading) for section in sectionpath]) for sectionpath in p.flat_headings_list()])
+                #print('flat headings= ', ["/".join([str(section.heading) for section in sectionpath]) for sectionpath in p.flat_headings_list()])
+                queries += [" ".join([str(section.heading) for section in sectionpath]) for sectionpath in p.flat_headings_list()]
 
             if counter >= limit:
                 break
 
             counter += 1
 
+    #print(queries)
+    return queries
+
 
 def preprocess_paragraphs(path=WikiParagrahs.file_path_list[0], limit=1):
+
+    paragraphs = []
 
     print('*** reading {} paragraph from file: {} ***'.format(limit, path))
     with open(path, 'rb') as f:
@@ -35,21 +43,25 @@ def preprocess_paragraphs(path=WikiParagrahs.file_path_list[0], limit=1):
 
         for p in iter_paragraphs(f):
 
-            print('----------------------- PARA ID  -----------------------')
-            print(p.para_id)
+            # print('----------------------- PARA ID  -----------------------')
+            # print(p.para_id)
+            #
+            # print('----------------------- PARA TEXT  -----------------------')
 
-            print('----------------------- PARA TEXT  -----------------------')
+            text = [elem.text if isinstance(elem, ParaText) else elem.anchor_text for elem in p.bodies]
+            S = [s.split(' ')[0] for s in text]
+            print(S)
 
-            texts = [elem.text if isinstance(elem, ParaText)
-                     else elem.anchor_text
-                     for elem in p.bodies]
-
-            print(texts)
+            paragraphs += S
 
             if counter >= limit:
                 break
 
             counter += 1
+
+    #print(paragraphs)
+    return paragraphs
+
 
 
 
--- main.py
+++ main.py
@@ -5,16 +5,65 @@ from data_exploration.printing_data import print_paragraphs, print_articles
 from data_exploration.preprocessing import preprocess_paragraphs, preprocess_queries
 
 from utils.static import Wiki, WikiParagrahs, Train, Benchmark
+from rank_bm25 import BM25Okapi
+
 
 if __name__ == "__main__":
-    download_and_unpack_dataset(D=Benchmark)
+    #download_and_unpack_dataset(D=Benchmark)
 
     #print_paragraphs(path=WikiParagrahs.file_path_list[0], limit=1)
     #print_articles(path=Wiki.file_path_list[4], limit=1)
 
-    #preprocess_queries(path=Wiki.file_path_list[0], limit=10)
+    #queries = preprocess_queries(path=Wiki.file_path_list[0], limit=100000)
+    #print(len(queries))
 
-    #preprocess_paragraphs(path=WikiParagrahs.file_path_list[0], limit=10)
+    paragraphs = preprocess_paragraphs(path=WikiParagrahs.file_path_list[0], limit=1)
+    print(paragraphs)
+    #print(len(paragraphs))
+
+    # print('tokenized corpus')
+    # tokenized_corpus = [doc.split(" ") for doc in paragraphs][0]
+    # print(tokenized_corpus)
+    #
+    # print('tokenized corpus')
+    # bm25 = BM25Okapi(tokenized_corpus)
+    #
+    # #tokenized_query = queries[0].split(" ")
+    # tokenized_query = ['Bay']
+    # print(tokenized_query)
+    #
+    # doc_scores = bm25.get_scores(tokenized_query)
+    # print(doc_scores)
+
+
+    # corpus = [
+    #     "Hello there good man!",
+    #     "It is quite windy in London",
+    #     "How is the weather today?"
+    # ]
+    #
+    # tokenized_corpus = [doc.split(" ") for doc in corpus]
+    # print(tokenized_corpus)
+    #
+    # bm25 = BM25Okapi(tokenized_corpus)
+    #
+    # query = "windy London"
+    # tokenized_query = query.split(" ")
+    #
+    # print(tokenized_query)
+    #
+    # doc_scores = bm25.get_scores(tokenized_query)
+    #
+    # print(doc_scores)
+    #
+    # print(bm25.get_top_n(tokenized_query, corpus, n=1))
+
+
+
+
+
+
+
 
 
 
