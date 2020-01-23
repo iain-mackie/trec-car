a76ad36f314ddacd0f2b012e8ade3b179b698c89
iain-mackie <i.mackie@me.com>
2020-01-23 11:28:58 +0000

    main updated with utf-8 parser

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
-from data_download.download import download_and_unpack_dataset
-from data_download.static import WikiParagrahs, Wiki, dataset_list
+from data_download.download import download_and_unpack_dataset, download_and_unpack_datasets
+from data_download.static import WikiParagrahs, Wiki, dataset_list, Train, Benchmark
 
 from index.static import write_q_path, write_d_path, write_q_norm_path, write_d_norm_path, write_rank_path
 from index.build_q_and_d import build_q, build_d
@@ -10,18 +10,40 @@ from index.ranking import retrieve_n_documents
 
 if __name__ == "__main__":
 
-    #download_and_unpack_dataset(D=Wiki)
-    #
-    build_q(read_path=Wiki.file_path_list[5], write_path=write_q_path, page_limit=500)
+    import os
 
-    build_d(read_path=WikiParagrahs.file_path_list[0], write_path=write_d_path, paragraph_limit=100000)
+    in_path = os.path.join(os.getcwd(), 'train.topics')
+    out_path = os.path.join(os.getcwd(), 'train_out.topics')
 
-    normalise_q(read_path=write_q_path, write_path=write_q_norm_path)
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    alpha = ['A', 'B', 'C', 'D', 'E', 'F']
+    valid_char = numbers + alpha
+    hex_utc8 = []
+    for c1 in valid_char:
+        for c2 in valid_char:
+            hex_utc8.append(c1 + c2)
 
-    normalise_d(read_path=write_d_path, write_path=write_d_norm_path)
+    with open(in_path, "r") as f:
+        lines = f.readlines()
 
-    retrieve_n_documents(read_path=write_q_norm_path, write_path=write_rank_path,  n=5)
-
+    with open(out_path, "w") as f:
+        for line in lines:
+            if '%' in line:
+                new_line = ''
+                counter = 0
+                for l in line:
+                    if l == '%':
+                        if line[counter+1:counter + 3] in hex_utc8:
+                            new_line += l
+                        else:
+                            print(line[counter:counter + 3])
+                            new_line += '%25'
+                    else:
+                        new_line += l
+                    counter += 1
+                f.write(new_line)
+            else:
+                f.write(line)
 
 
 
